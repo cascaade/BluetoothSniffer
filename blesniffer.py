2307352a754465a0e97854ef79f5924fe4a384f2
Owen <189063419+cascaade@users.noreply.github.com>
2025-12-28 13:45:22 -0500

    Get data and communicate with device

--- blesniffer.py
+++ blesniffer.py
@@ -1,10 +1,61 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BLEDevice, AdvertisementData, BleakClient
+
+DEVICE_NAME = "BT05"
+SERVICE_UUID = "0000ffe0-0000-1000-8000-00805F9B34FB"
+CHAR_UUID = "0000ffe1-0000-1000-8000-00805F9B34FB"
+
+def getDeviceDataFromDevice(device):
+    bledevice: BLEDevice
+    advdata: AdvertisementData
+
+    bledevice, advdata = device
+
+    d = {
+        "device": bledevice,
+        "name": bledevice.name,
+        "details": bledevice.details,
+        "address": bledevice.address,
+        "local_name": advdata.local_name,
+        "manufacturer_data": advdata.manufacturer_data,
+        "platform_data": advdata.platform_data,
+        "rssi": advdata.rssi,
+        "service_data": advdata.service_data,
+        "service_uuids": advdata.service_uuids,
+        "tx_power": advdata.tx_power
+    }
+
+    return d
 
 async def main():
-    devices = await BleakScanner.discover()
+    devices = await BleakScanner.discover(return_adv=True)
+
+    bt05: BLEDevice
 
     for device in devices:
-        print(device)
+        data = getDeviceDataFromDevice(devices[device])
 
+        if data.get("name") == DEVICE_NAME:
+            bt05 = data.get("device")
+            break
+
+    if not bt05:
+        return
+    
+    async with BleakClient(bt05, pair=True) as client:
+        print("is connected?: " + str(client.is_connected))
+
+        while True:
+            q = input("data: ")
+
+            if q.lower() == "quit":
+                break
+
+            q += "\n\r"
+
+            await client.write_gatt_char(CHAR_UUID, q.encode(), response=False)
+
+    
+    print("device disconnected. program quitting.")
+    
 asyncio.run(main())
